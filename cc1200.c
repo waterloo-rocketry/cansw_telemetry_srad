5ac1711496fe0883835b9176fc68d50aa632133d
Rio Liu <rio@r26.me>
2026-07-03 19:10:31 -0400

    fix CC1200 GPIO mapping

--- cc1200.c
+++ cc1200.c
@@ -76,9 +76,9 @@ static const registerSetting_t preferredSettings[] = {
     {CC1200_XOSC1,          0x03},
 
     // manual configs
-    {CC1200_IOCFG3,         0x5A}, // GPIO3 to RX0TX1_CFG, inverted
+    {CC1200_IOCFG3,         0x24}, // GPIO3 to ANTENNA_SELECT
     {CC1200_IOCFG2,         0x07}, // GPIO2 to CRC_OK
-    {CC1200_IOCFG0,         0x24}, // GPIO0 to ANTENNA_SELECT
+    {CC1200_IOCFG0,         0x5A}, // GPIO0 to RX0TX1_CFG, inverted
     {CC1200_FREQOFF1,       0x02}, // Frequency Offset MSB
     {CC1200_FREQOFF0,       0xB6}, // Frequency Offset LSB
     {CC1200_RFEND_CFG1,     0x0E}, // RXOFF_MODE = IDLE, RX_TIME = disable
